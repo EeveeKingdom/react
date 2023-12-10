168c2e91cb3a1aa9fdd925b0e9955358160cb5dd
EeveeKingdom <57893006+EeveeKingdom@users.noreply.github.com>
2023-12-11 00:27:27 +0800

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+// function App() {
+//     return <></>
+// }
+
+createRoot(document.getElementById("root")!).render(<App />);
+
+// import { createRoot } from "react-dom/client";
+// import "animate.css";
+// import "@/style/global.scss";
+// import App from "./App";
+
+// createRoot(document.getElementById("root")!).render(<App />);
